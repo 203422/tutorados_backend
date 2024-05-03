84ace136ca6d90c6c46294eea70f6ffc7e6e0c20
203422 <203422@ids.upchiapas.edu.mx>
2024-05-02 23:11:19 -0600

    Añadiendo proyecto

--- src/db/database.js
+++ src/db/database.js
@@ -3,7 +3,7 @@ console.log(process.env);
 export const pool = mysql.createPool({
     host: process.env.HOST ?? "",
     port: process.env.PORT_DATABASE ?? "",
-    user: process.env.USER ?? "",
+    user: process.env.USER_DATABASE ?? "",
     password: process.env.PASSWORD ?? "",
     database: process.env.DATABASE ?? "",
 });
